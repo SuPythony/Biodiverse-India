ed38fbe7554ae6c541dea533925d287d77ff56e2
Sumanyu Aggarwal <sumanyu.code@gmail.com>
2021-11-30 12:32:35 +0530

    updated

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -7,7 +7,6 @@
 	import { fly } from "svelte/transition";
 
 	let thisIndex = index++;
-
 	export let title: string = "Title";
 </script>
 
@@ -16,7 +15,7 @@
 		in:fly={{ x: $prevIndex < thisIndex ? 100 : -100 }}
 		class="bg-cardBg rounded-md md:min-h-3/4 min-h-1/2 p-5 pt-2 md:w-3/6 w-4/5 break-words flex justify-top flex-col items-center mt-auto mb-auto"
 	>
-		<h1 class="font-bold text-4xl">{title}</h1>
+		<h1 class="font-bold text-4xl" style="color: rgb(50, 220, 255);">{title}</h1>
 		<hr class="w-11/12 border-hrColor mt-2" />
 		<div class="mt-3 w-full">
 			<slot />
